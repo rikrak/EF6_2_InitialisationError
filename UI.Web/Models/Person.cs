fe65b171d56825bd72963eaf27ebf3ee31f3fcb5
Rik Crompton <rik@rikandwigs.com>
2017-11-10 09:29:58 +0000

    Initial code

---
+++ UI.Web/Models/Person.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Model
+{
+    public class Person
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(120)]
+        public string GivenName { get; set; }
+
+        [Required]
+        [StringLength(120)]
+        public string FamilyName { get; set; }
+    }
+}
---
+++ UI.Web/UnitOfWork/SampleContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+using System.Security.Cryptography.X509Certificates;
+using Model;
+
+namespace UI.Web.UnitOfWork
+{
+    public class SampleContext : DbContext
+    {
+        public SampleContext() : base("DefaultConnection")
+        {
+            
+        }
+
+        public DbSet<Person> Person { get; set; }
+    }
+}
